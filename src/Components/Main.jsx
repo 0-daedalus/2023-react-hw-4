9027429cdf57f0cb700a14769879be0376d82166
0-daedalus <armageddon_WO@mail.ru>
2023-06-09 15:33:01 +0600

    Delete Api comp, add client-side rendering

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,16 +1,32 @@
 /* eslint-disable react/react-in-jsx-scope */
+"use client"
 import Image from "next/image";
-import Api from "./Api";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const Main = () =>{
+    const [display, setDisplay] = useState(true);
+    const [data,setData] = useState([]);
+    useEffect(() => {
+        const getData = async () =>{
+            const res = await axios.get("https://api.kanye.rest");
+            setData(res.data.quote);
+        }
+        getData()
+    }, [display])
+    
     return (<div className="flex-1 bg-gray-100 flex justify-center items-center">
-        <div className="flex flex-col gap-5">
-            <Image 
-            src="/6960604_preview.jpeg" 
-            width={820}
-            height={619}
-            className="block"
-            ></Image>
-            <Api />
+        <div className="flex flex-col items-center gap-5">
+            <button onClick={() => {
+                setDisplay(!display);
+            }}>
+                <Image 
+                src="/pngegg.png" 
+                width={467}
+                height={620}
+                className="block active:drop-shadow-[0_35px_35px_rgba(255,0,0,0.5)]"
+                ></Image>
+            </button>
+            <div className="text-center text-xl font-bold">{data}</div>
         </div>
     </div>
     )
